4a7f0f6f07fb26ed247d78cbb80afbabee10d75b
Zvorky <enzzavorski@edu.univali.br>
2023-03-10 13:43:59 -0300

    Create Caligrafo.py

---
+++ Caligrafo.py
@@ -0,0 +1,22 @@
+#! /usr/bin/python3
+
+#	Caligrafo.py
+
+''' Enzo Zavorski Delevatti
+||| @Zvorky
+\\\          ___,
+ \\\      .~´    `-,
+  \\°    /  _    _ \.
+   \°   ,\`|_|''|_|´\
+    °    /          /)   °
+        (\  ,    , .\`   |°
+         `) ;`,; `,^,)   ||°
+         ´,´  `,  `  `   |||
+                          \\\
+        March     2023     |||
+                           '''
+
+
+
+
+import os
